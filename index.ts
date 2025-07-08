329a4e60ef9a0cbb3a8812f6e2cc5dafc76086cf
narayan-mindfire <narayanp@mindfiresolutions.com>
2025-07-08 09:15:52 +0530

    feat: add basic route structure

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {Express, Request, Response} from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db"
+import appointmentsRouter from "./routes/appointments"
 dotenv.config()
 const port = process.env.PORT
 
@@ -9,6 +10,8 @@ const app:Express = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+app.use("/appointments", appointmentsRouter)
 
 app.get("/", (req:Request, res:Response) => {
     res.send("hello from express, this is written in ts")
